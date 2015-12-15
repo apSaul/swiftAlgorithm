b4e23e6d3dfc21cdf3b78c310287200c2004d0e2
Saul Zamarripa <saul.zamarripa.rivera@gmail.com>
2015-12-15 11:29:13 -0600

    Create swift_factorial.swift
    Creación de ejemplo de factorial de n con swift

---
+++ swift_factorial.swift
@@ -0,0 +1,8 @@
+// n!: factorial de 'n' en swift
+
+func factorial (n: Int) -> Int{
+    return n > 0 ? n * factorial(n-1) : 1
+}
+
+var n: Int = 6
+print ("el factorial de \(n) es \(factorial(n))")
